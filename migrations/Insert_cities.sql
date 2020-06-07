d30ad9bc51798323da7cda3b20358c5ba1c05aad
kokomal9 <kokomal@abv.bg>
2020-06-07 17:55:03 +0300

    Add city generator

---
+++ migrations/Insert_cities.sql
@@ -0,0 +1,8 @@
+insert into local.city (name) values ('Благоевград');
+insert into local.city (name) values ('София');
+insert into local.city (name) values ('Бургас');
+insert into local.city (name) values ('Шумен');
+insert into local.city (name) values ('Пловдив');
+insert into local.city (name) values ('Варна');
+insert into local.city (name) values ('Русе');
+insert into local.city (name) values ('Габрово');
